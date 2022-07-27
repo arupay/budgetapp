5e2f3f9672df438f347e7bf10383f522eeaa16dc
arupay <arupay11374@gmail.com>
2022-07-27 11:24:43 -0400

    two graphs

--- src/Components/Visuals.js
+++ src/Components/Visuals.js
@@ -32,72 +32,135 @@ export default function Visuals() {
     return { ...cat, value: totalByCategory(cat.name) };
   });
   const expenses = updatedCategories.filter((cat) => cat.type === "expense");
-  // const income = updatedCategories.filter((cat) => cat.type === "income");
+  const income = updatedCategories.filter((cat) => cat.type === "income");
 
-  const width = 400;
+  const width = 300;
   const half = width / 2;
 
   return (
-    <Container className="text-center pt-5">
-      <section>
-        <svg width={width} height={width}>
-          <Group top={half} left={half}>
-            <Pie
-              // data={updatedCategories.filter((e) => (e.type = "expense"))}
-              data={expenses}
-              pieValue={(data) => data.value}
-              outerRadius={half}
-              innerRadius={({ data }) => {
-                // console.log({ data }); ISOLATED DATA , set up ternary to conditionally render w/ hover effect
-                const size = active && active.name === data.name ? 25 : 15;
-                return half - size;
-              }}
-              padAngle={0.01}
-            >
-              {(pie) => {
-                return pie.arcs.map((arc) => {
-                  return (
-                    <g
-                      key={arc.data.name}
-                      onMouseEnter={() => setActive(arc.data)}
-                      onMouseLeave={() => setActive(null)}
-                    >
-                      <path d={pie.path(arc)} fill={arc.data.color}></path>
-                    </g>
-                  );
-                });
-                //consoled logged arc to see an object, each element in data
-              }}
-            </Pie>
-            {active ? (
-              <>
-                <Text textAnchor="middle" fontSize={30} dy={-20} fill="#000">
-                  {`$${active.value}`}
-                </Text>
-                <Text
-                  textAnchor="middle"
-                  fill={active.color}
-                  fontSize={20}
-                  dy={20}
-                >
-                  {active.name}
-                </Text>
-              </>
-            ) : (
-              <>
-                <Text textAnchor="middle" fontSize={30} dy={-20}>
-                  {`$${expenses.reduce((acc, ele) => acc + ele.value, 0)}`}
-                </Text>
-                <Text
-                  textAnchor="middle"
-                  fontSize={20}
-                  dy={20}
-                >{`Total Expenses:  ${expenses.length} Sources`}</Text>
-              </>
-            )}
-          </Group>
-        </svg>
-      </section>
-    </Container>
+    <div className="row">
+      <Container className="col-md-4 pt-5">
+        <section>
+          <svg width={width} height={width}>
+            <Group top={half} left={half}>
+              <Pie
+                // data={updatedCategories.filter((e) => (e.type = "expense"))}
+                data={expenses}
+                pieValue={(data) => data.value}
+                outerRadius={half}
+                innerRadius={({ data }) => {
+                  // console.log({ data }); ISOLATED DATA , set up ternary to conditionally render w/ hover effect
+                  const size = active && active.name === data.name ? 25 : 15;
+                  return half - size;
+                }}
+                padAngle={0.01}
+              >
+                {(pie) => {
+                  return pie.arcs.map((arc) => {
+                    return (
+                      <g
+                        key={arc.data.name}
+                        onMouseEnter={() => setActive(arc.data)}
+                        onMouseLeave={() => setActive(null)}
+                      >
+                        <path d={pie.path(arc)} fill={arc.data.color}></path>
+                      </g>
+                    );
+                  });
+                  //consoled logged arc to see an object, each element in data
+                }}
+              </Pie>
+              {active ? (
+                <>
+                  <Text textAnchor="middle" fontSize={30} dy={-20} fill="#000">
+                    {`$${active.value}`}
+                  </Text>
+                  <Text
+                    textAnchor="middle"
+                    fill={active.color}
+                    fontSize={20}
+                    dy={20}
+                  >
+                    {active.name}
+                  </Text>
+                </>
+              ) : (
+                <>
+                  <Text textAnchor="middle" fontSize={30} dy={-20}>
+                    {`$${expenses.reduce((acc, ele) => acc + ele.value, 0)}`}
+                  </Text>
+                  <Text
+                    textAnchor="middle"
+                    fontSize={20}
+                    dy={20}
+                  >{`Total Expenses:  ${expenses.length} Sources`}</Text>
+                </>
+              )}
+            </Group>
+          </svg>
+        </section>
+      </Container>
+      <Container className="col-md-4 pt-5">
+        <section>
+          <svg width={width} height={width}>
+            <Group top={half} left={half}>
+              <Pie
+                // data={updatedCategories.filter((e) => (e.type = "expense"))}
+                data={income}
+                pieValue={(data) => data.value}
+                outerRadius={half}
+                innerRadius={({ data }) => {
+                  // console.log({ data }); ISOLATED DATA , set up ternary to conditionally render w/ hover effect
+                  const size = active && active.name === data.name ? 25 : 15;
+                  return half - size;
+                }}
+                padAngle={0.01}
+              >
+                {(pie) => {
+                  return pie.arcs.map((arc) => {
+                    return (
+                      <g
+                        key={arc.data.name}
+                        onMouseEnter={() => setActive(arc.data)}
+                        onMouseLeave={() => setActive(null)}
+                      >
+                        <path d={pie.path(arc)} fill={arc.data.color}></path>
+                      </g>
+                    );
+                  });
+                  //consoled logged arc to see an object, each element in data
+                }}
+              </Pie>
+              {active ? (
+                <>
+                  <Text textAnchor="middle" fontSize={30} dy={-20} fill="#000">
+                    {`$${active.value}`}
+                  </Text>
+                  <Text
+                    textAnchor="middle"
+                    fill={active.color}
+                    fontSize={20}
+                    dy={20}
+                  >
+                    {active.name}
+                  </Text>
+                </>
+              ) : (
+                <>
+                  <Text textAnchor="middle" fontSize={30} dy={-20}>
+                    {`$${income.reduce((acc, ele) => acc + ele.value, 0)}`}
+                  </Text>
+                  <Text
+                    textAnchor="middle"
+                    fontSize={20}
+                    dy={20}
+                  >{`Total Income:  ${income.length} Sources`}</Text>
+                </>
+              )}
+            </Group>
+          </svg>
+        </section>
+      </Container>
+    </div>
   );
 }
